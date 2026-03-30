7aedfa4d8f2ff1b41e86b1e707b2c9df16d53070
Matheus de Souza <matheusgds3011@gmail.com>
2026-03-30 00:32:21 -0300

    Revisando e refatorando

--- app/index.js
+++ app/index.js
@@ -1,63 +1,59 @@
+const html = document.querySelector('html');
 const btnFoco = document.querySelector('.app__card-button--foco');
 const btnCurto = document.querySelector('.app__card-button--curto');
 const btnLongo = document.querySelector('.app__card-button--longo');
-const html = document.querySelector('html');
 const titulo = document.querySelector('.app__title');
 const imgPrincipal = document.querySelector('.app__image');
-const inputMusica = document.getElementById('alternar-musica');
-const musica = new Audio('/sons/luna-rise-part-one.mp3');
-musica.loop = true;
 
-inputMusica.addEventListener('change', () => {
-    if(musica.paused){
-        musica.play();
-    }else {
-        musica.pause();
-    }
-})
-
-function alteraVisualizacaoDaPagina(parametroHtml, srcImg){
-    html.setAttribute('data-contexto', parametroHtml);
-    imgPrincipal.src = srcImg;
-    switch(parametroHtml){
+function alteraVisualDaPagina(atributoHtml){
+    html.setAttribute('data-contexto', atributoHtml);
+    switch(atributoHtml){
         case 'foco':
             titulo.innerHTML = 'Otimize sua produtividade,<br><strong class="app__title-strong">mergulhe no que importa.</strong>';
+            imgPrincipal.src = '/imagens/foco.png';
+            btnFoco.classList.add('active');
+            btnCurto.classList.remove('active');
+            btnLongo.classList.remove('active');
             break;
-        
         case 'descanso-curto':
             titulo.innerHTML = 'Que tal dar uma respirada?<br><strong class="app__title-strong">Faça uma pausa curta!</strong>';
+            imgPrincipal.src = '/imagens/descanso-curto.png';
+            btnCurto.classList.add('active');
+            btnFoco.classList.remove('active');
+            btnLongo.classList.remove('active');
             break;
-
         case 'descanso-longo':
             titulo.innerHTML = 'Hora de voltar à superficie.<br><strong class="app__title-strong">Faça uma pausa longa.</strong>';
+            imgPrincipal.src = '/imagens/descanso-longo.png';
+            btnLongo.classList.add('active');
+            btnFoco.classList.remove('active');
+            btnCurto.classList.remove('active');
             break;
-
         default:
             break;
     }
 }
 
 btnFoco.addEventListener('click', () => {
-    alteraVisualizacaoDaPagina('foco', '/imagens/foco.png');
-
-    btnFoco.classList.add('active');
-    btnCurto.classList.remove('active');
-    btnLongo.classList.remove('active');
-})
+    alteraVisualDaPagina('foco');
+});
 
 btnCurto.addEventListener('click', () => {
-    alteraVisualizacaoDaPagina('descanso-curto', '/imagens/descanso-curto.png');
-
-    btnCurto.classList.add('active');
-    btnFoco.classList.remove('active');
-    btnLongo.classList.remove('active');
-})
+    alteraVisualDaPagina('descanso-curto');
+});
 
 btnLongo.addEventListener('click', () => {
-    alteraVisualizacaoDaPagina('descanso-longo', '/imagens/descanso-longo.png');
+    alteraVisualDaPagina('descanso-longo');
+});
 
-    btnLongo.classList.add('active');
-    btnFoco.classList.remove('active');
-    btnCurto.classList.remove('active');
-})
+const inputMusica = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+musica.loop = true;
 
+inputMusica.addEventListener('change', () => {
+    if(musica.paused){
+        musica.play();
+    } else {
+        musica.pause();
+    }
+});
